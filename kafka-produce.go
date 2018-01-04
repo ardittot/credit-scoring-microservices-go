89cccf4d6e826add7e123cb6dcc296808df8d2ac
Arditto Trianggada <atrianggada@gmail.com>
2018-01-04 14:25:26 +0700

    Update kafka-produce.go

--- kafka-produce.go
+++ kafka-produce.go
@@ -80,8 +80,8 @@ func consumeKafka() (out Las_status_array) {
 
 			switch e := ev.(type) {
 			case *kafka.Message:
-				//json.Unmarshal(e.Value, out)
 				fmt.Printf("%% Message on %s:\n%s\n", e.TopicPartition, string(e.Value))
+				json.Unmarshal(e.Value, &out)
 			case kafka.PartitionEOF:
 				fmt.Printf("%% Reached %v\n", e)
 			case kafka.Error:
