5145c1cfde5d40e226a726498ba08ac4f9d9989a
Arditto Trianggada <atrianggada@gmail.com>
2018-01-04 13:42:44 +0700

    Update kafka-produce.go

--- kafka-produce.go
+++ kafka-produce.go
@@ -8,14 +8,36 @@ import (
 )
 
 var producer *kafka.Producer
+var consumer *kafka.Consumer
 var broker string
 //var topic string
 
-func InitKafka() (err error) {
+func InitKafkaProducer() (err error) {
 	broker = "10.148.0.4:9092"
 	producer, err = kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": broker})
 	if err!=nil{
 		os.Exit(1)
+	}
+	return
+}
+
+func InitKafkaConsumer() (err error) {
+	broker = "10.148.0.4:9092"
+	group = "test2-group"
+	topic := "test2"
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers":               broker,
+		"group.id":                        group,
+		"session.timeout.ms":              6000,
+		"go.events.channel.enable":        true,
+		"go.application.rebalance.enable": true,
+		"default.topic.config":            kafka.ConfigMap{"auto.offset.reset": "earliest"}})	
+	if err!=nil{
+		os.Exit(1)
+	}
+	err = consumer.SubscribeTopics(topic, nil)
+	if err!=nil{
+		os.Exit(2)
 	}
 	return
 }
@@ -34,5 +56,40 @@ func (out Las_status) ProduceKafka() {
 		}
 		close(deliveryChan)
 	}
+}
+
+func (out *Las_status) ConsumeKafka() {
+	topic := "test2"
+	
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	
+	run := true
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := consumer.Poll(100)
+			if ev == nil {
+				continue
+			}
+
+			switch e := ev.(type) {
+			case *kafka.Message:
+				//fmt.Printf("%% Message on %s:\n%s\n", e.TopicPartition, string(e.Value))
+				json.Unmarshal(e.Value, out)
+			case kafka.PartitionEOF:
+				fmt.Printf("%% Reached %v\n", e)
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+				run = false
+			default:
+				fmt.Printf("Ignored %v\n", e)
+			}
+		}
+	}
+
 }
 
