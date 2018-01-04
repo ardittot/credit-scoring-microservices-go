a06aa9a4d56a812c1bfb3b402d5e48e13463f4a2
Arditto Trianggada <atrianggada@gmail.com>
2018-01-04 15:41:37 +0700

    Update main.go

--- main.go
+++ main.go
@@ -18,10 +18,10 @@ func main() {
   // Initialize kafka
   InitKafkaProducer()
   InitKafkaConsumer()
-  go func() { 
-	out := consumeKafka()
-	fmt.Printf("%% Message :\n%s\n%s\n", out[0].ID_Scoring, out[0].Score)
-  }()
+  //go func() { 
+	//out := consumeKafka()
+	//fmt.Printf("%% Message :\n%s\n%s\n", out[0].ID_Scoring, out[0].Score)
+  //}()
 
   // Initialize the routes
   initializeRoutes()
